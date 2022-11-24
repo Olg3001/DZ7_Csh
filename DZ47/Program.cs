5fc3596ce746b8cecc3ed440d67d8c1ec3b79bf2
Olga <olg3001@yandex.ru>
2022-11-24 19:12:44 +0300

    DZ47

---
+++ DZ47/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными 
+// вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+double[,] CreateArray(int rows, int columns)
+{
+    Random random = new Random();
+
+    double[,] array = new double[rows, columns];
+    
+    for(int i = 0; i < rows; i++)
+    {
+        for(int j = 0; j < columns; j++)
+        {
+            array[i, j] = random.Next(-100, 101) / 10.0;
+        }
+    }
+    
+    return array;
+}
+
+void PrintArray(double[,] array)
+{
+    Console.WriteLine("Created Array:");
+
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write("{0,6}", array[i, j]);
+        }
+
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Programm to create the random 2D array with random real numbers.");
+Console.WriteLine("Press 'Enter' after every inputed number");
+Console.Write("Input the rows number: ");
+int rows;
+
+while(!int.TryParse(Console.ReadLine()!, out rows) || rows <= 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Wrong input");
+    Console.ResetColor();
+    Console.Write("Input the rows number: ");
+}
+
+Console.Write("Input the columns number: ");
+int columns;
+
+while(!int.TryParse(Console.ReadLine()!, out columns) || columns <= 0)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Wrong input");
+    Console.ResetColor();
+    Console.Write("Input the colums number: ");
+}
+
+var array = CreateArray(rows, columns);
+PrintArray(array);
